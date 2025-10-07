bc938b748a5121ae6c03f6ac6e97b1df678b50eb
unknown <you@example.com>
2025-10-07 15:34:17 -0300

    lojinha

--- SRC/controllers/produtoController.js
+++ SRC/controllers/produtoController.js
@@ -15,6 +15,36 @@ const produtoController = {
             console.error('erro ao listar produtos:', error);
             res.status(500).json({message: 'Error ao buscar produto'})
         }
+    },
+
+    // -----------------------//
+    //Criar um novo produto---//
+    //Post /produtos----------//
+    /*
+        {
+            "nomeProduto": "valor",
+            "precoProduto": 0.00
+        }
+    */
+    //------------------------//
+
+    criarProduto: async (req, res)=>{
+        try {
+            
+            const {nomeProduto, precoProdutor} = req.body;
+            
+            if(nomeProduto == undefined || precoProdutor == undefined || isNaN(precoProdutor)){
+                return res.status(400).json({erro: 'campos OBRIGATÓRIOS não preenchidos'})
+            }
+   
+            await produtoModel.inserirProduto(nomeProduto, precoProdutor);
+
+            res.status(201).json({message: 'produto cadastrado com sucesso!'});
+
+        } catch (error) {
+            console.error('erro ao cadastrar o produto!', error)
+            res.status(500).json({error:'erro no servidor ao cadastrar o produto'});
+        }
     }
 }
 
--- SRC/models/produtoModel.js
+++ SRC/models/produtoModel.js
@@ -16,6 +16,24 @@ const produtoModel = {
                 console.error('error ao buscar produtos', error)
                 throw error; //passa o erro para controller tratar
         }
+    },
+
+    inserirProduto: async (nomeProduto, precoProdutor)=>{
+        try {
+
+            const pool = await getConnection();
+
+            let querySQL = 'INSERT INTO produtos(nomeProduto, precoProdutor) VALUES (@nomeProduto, @precoProdutor)';
+
+            await pool.request()
+            .input('nomeProduto', sql.VarChar(100), nomeProduto)
+            .input('precoProdutor', sql.Decimal(10,2), precoProdutor)
+            .query(querySQL);
+            
+        } catch (error) {
+            console.error('Erro ao inserir o produto', error);
+            throw error; //passa o erro para controller tratar
+        }
     }
 }
 
